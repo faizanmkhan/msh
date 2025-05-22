fa15e2b7d9792bf49acde4619d08b5dfa1ca9088
Kacper <kacper.orzechowski.04@gmail.com>
2025-05-22 23:55:52 +0200

    sep paths in ex. funcs

--- exec_dir/external_cmd.c
+++ exec_dir/external_cmd.c
@@ -1,46 +1,93 @@
 #include "../minishell.h"
 
+int	chr_in_str(char *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (!str || !c)
+		return (-1);
+	while (str[i])
+		if (str[i++] == c)
+			count++;
+	return (count);
+}
+
+char	**fill_buffer_paths(char **paths, int count, char *str)
+{
+	int	last_occ;
+	int	i;
+	int	j;
+	int	len;
+
+	if (!str || !paths)
+		return (NULL);
+	len = ft_strlen(str);
+	i = -1;
+	j = -1;
+	last_occ = 0;
+	while (++i < count)
+	{
+		while (++j < len + 1)
+		{
+			if (str[j] == ':' || str[j] == '\0')
+			{
+				paths[i] = ft_substr(str, last_occ, (j) - last_occ);
+				last_occ = ++j;
+				break ;
+			}
+		}
+	}
+	paths[i] = NULL;
+	return (paths);
+}
+
 char	**separate_paths(char *str)
 {
 	char	**paths;
-	int	i;
-	int	j;
-	int	count;
-	int	last_occ;
-	int	start_occ;
+	int		count;
 
-	i = 0;
-	j = 0;
-	start_occ = 0;
-	count = 1;
-	while (str[j])
-		if (str[j++] == ':')
-			count++;
-	j = 0;
-	i = 0;
+	count = chr_in_str(str, ':') + 1;
 	paths = (char **)malloc(sizeof(char *) * (count + 1));
-	while (i <= count)
+	if (!paths || count < 1)
 	{
-		while (str[j])
-		{
-			if (str[j] == ':')
-			{
-				paths[i] = ft_substr(str, last_occ, 
-						//HERE GO FILLING THOSE PATHS!!! 
-		}
+		perror("Alloc error!");
+		return (NULL);
 	}
+	paths = fill_buffer_paths(paths, count, str);
+	if (!paths)
+	{
+		perror("Error");
+		return (NULL);
+	}
+	return (paths);
 }
-void	check_cmd()
-{
-	char	**paths;
-	char	*path_var;
-	int	len;
-	//if not buildin cmd found
-	path_var = ft_strdup(getenv("PATH"));
-}
+//void	check_cmd()
+//{
+//	char	**paths;
+//	char	*path_var;
+//	int	len;
+//	//if not buildin cmd found
+//	path_var = ft_strdup(getenv("PATH"));
+//}
 
-int	main()
-{
-	check_cmd();
+/*
+//int	main()
+//{
+//	//check_cmd();
+//	char *str = "test:dupa/ale/akscja:okey:bbbb";
+//	char **a = separate_paths(str);
+//	if (a == NULL)
+//		perror("ERROR!");
+	int i = 0;
+	while (a[i])
+	{
+		printf("path %d: %s\n", i, a[i]);
+		i++;
+	}
+	printf("path %d: %s\n", i, a[i]);
 	return (0);
 }
+*/
