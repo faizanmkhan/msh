58f147e34d893d60d7e02b51890d7c54d13cf695
Kacper <kacper.orzechowski.04@gmail.com>
2025-05-24 14:34:24 +0200

    buildin implementation

--- exec_dir/buildin_cmd.c
+++ exec_dir/buildin_cmd.c
@@ -7,6 +7,57 @@
 //4. 'global' variable support for export and unset functions.
 //5. Actually kill cmd i guess (kill_all func, that kills each pid from pid array in struct maybe??)
 
+int	ft_echo(char **args)
+{
+	int	i;
+	int	n_flag;
+
+	i = 1;
+	n_flag = 0;
+	if (!args)
+		return (-1);
+	else if (!args[1])
+	{
+		ft_putendl_fd("", 1);
+		return (0);
+	}
+	if (ft_strncmp(args[1], "-n", 2) == 0)
+	{
+		n_flag = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		if (n_flag == 1 && i > 2)
+			ft_putchar_fd(' ', 1);
+		else if (n_flag == 0 && i > 1)
+			ft_putchar_fd(' ', 1);
+		if (n_flag == 0 && !args[i + 1])
+			ft_putendl_fd(args[i], 1);
+		else
+			ft_putstr_fd(args[i], 1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_cd(char *path, t_shell *shell)
+{
+	char	*cwd;
+
+	cwd = shell->current_dir;
+	if (!path)
+		chdir(getenv("HOME"));
+	else if (chdir(path) == -1)
+	{
+		ft_putstr_fd("cd: no such file or directory: ", STD_ERR);
+		ft_putendl_fd(path, STD_ERR);
+		return (-1);
+	}
+	getcwd(cwd, 1024);
+	return (0);
+}
+
 int	perform_buildin(char **cmds, char *path)
 {
 	pid_t	pid;
@@ -20,26 +71,35 @@ int	perform_buildin(char **cmds, char *path)
 		printf("Parent\n");
 }
 
-int	check_is_buildin(char **cmds)
+int	ft_pwd (char **args, t_shell *shell)
+{
+	if (args[1])
+	{
+		ft_putendl_fd("pwd: too many arguments", STD_ERR);
+		return (-1);
+	}
+	ft_putendl_fd(shell->current_dir, 1);
+	return (0);
+}
+
+int	check_is_buildin(char **cmds, t_shell *shell)
 {
 	char *path;
-	
-	if (!cmds || !cmds[0])
-		return (-1);
-	path = ft_strjoin("/bin/", cmds[0]);
 
+	if (!cmds || !cmds[0] || !shell || !shell->current_dir)
+		return (-1);
 	if (ft_strncmp(cmds[0], "echo", 4) == 0)
-		return (perform_buildin(cmds, path));
+		return(ft_echo(cmds));
 	else if (ft_strncmp(cmds[0], "cd", 2) == 0)
-		return (chdir("/home/kacper/"));
+		return (ft_cd(cmds[1], shell));
 	else if (ft_strncmp(cmds[0], "pwd", 3) == 0)
-		return (perform_buildin(cmds, path));
-	else if (ft_strncmp(cmds[0], "export", 6) == 0)
-		return (perform_buildin(cmds, path));
-	else if (ft_strncmp(cmds[0], "unset", 5) == 0)
-		return (perform_buildin(cmds, path));
-	else if (ft_strncmp(cmds[0], "env", 3) == 0)
-		return (perform_buildin(cmds, path));
+		return (ft_pwd(cmds, shell));
+	//else if (ft_strncmp(cmds[0], "export", 6) == 0)
+	//	return (perform_buildin(cmds, path));
+	//else if (ft_strncmp(cmds[0], "unset", 5) == 0)
+	//	return (perform_buildin(cmds, path));
+	//else if (ft_strncmp(cmds[0], "env", 3) == 0)
+	//	return (perform_buildin(cmds, path));
 	//else if (ft_strncmp(cmds[0], "exit", 4) == 0)
 		//return (kill()); //
 	else
@@ -48,26 +108,38 @@ int	check_is_buildin(char **cmds)
 }
 
 #include <stdlib.h>
-
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	char **a;
+	int	return_code;
+    char **a;
+    int i;
+	t_shell	*shell;
 
-	(void)argc; // Unused for now
+	return_code = 0;
+	shell = (t_shell *)malloc(sizeof(t_shell));
+	shell->current_dir = (char *)malloc(sizeof(char) * 1024);
+	getcwd(shell->current_dir, 1024);
+	// Skip if there are no arguments besides program name
+    if (argc < 2)
+    {
+        printf("Usage: %s <command> [args...]\n", argv[0]);
+        return (1);
+    }
 
-	a = (char **)malloc(sizeof(char *) * 3);
-	if (!a)
-		return (1);
+    // Allocate space for all arguments except program name, plus NULL terminator
+    a = (char **)malloc(sizeof(char *) * argc);
+    if (!a)
+        return (1);
 
-	a[0] = argv[1];  // Example: "./minishell" or whatever the binary name is
-	a[1] = argv[2];
-	a[2] = NULL;
-	// NULL-terminate the array
+    // Copy argv[1] ... argv[argc-1] into a[0] ... a[argc-2]
+    for (i = 1; i < argc; i++)
+        a[i - 1] = argv[i];
+    a[argc - 1] = NULL; // NULL-terminate
 
+    return_code = check_is_buildin(a, shell);
 
-	check_is_buildin(a);  // Assuming this checks if a[0] is a built-in command
-
-	free(a);
-	return (0);
+    free(a);
+    free(shell->current_dir);
+    free(shell);
+    return (return_code);
 }
-
--- minishell.h
+++ minishell.h
@@ -1,9 +1,18 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# define STD_IN 0
+# define STD_OUT 1
+# define STD_ERR 2
+
 #include <stdlib.h>
 #include "exec_dir/Libft/srcs/libft.h"
 #include <unistd.h>
 #include <stdio.h>
+
+typedef	struct	s_shell
+{
+	char	*current_dir;
+}	t_shell;
 
 #endif
